169413d6dabed88231567a9bf990dba89b9e2e7e
AleksCreative <aleksdesign@yahoo.co.uk>
2023-06-23 16:12:51 +0100

    add archive.php, fix author link meta, lesson 24

--- single.php
+++ single.php
@@ -17,7 +17,7 @@ while(have_posts()) {
 
     <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-            <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog home</a><span class="metabox__main">Posted by <?php the_author(); ?> on <?php the_time('d F Y'); ?> in <?php echo get_the_category_list(', '); ?></span>
+            <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog home</a><span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('d F Y'); ?> in <?php echo get_the_category_list(', '); ?></span>
         </p>
     </div>
     <div class="generic-content">
