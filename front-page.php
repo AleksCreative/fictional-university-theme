4594a666331b2a1054a5b02e217706ca77a860c1
AleksCreative <aleksdesign@yahoo.co.uk>
2023-07-10 18:45:35 +0100

    order events on front page by their date

--- front-page.php
+++ front-page.php
@@ -16,7 +16,10 @@
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
           <?php $homepageEvents = new WP_Query(array(
             'posts_per_page' => 2,
-            'post_type' => 'event'
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC'
           ));
           while($homepageEvents->have_posts()) {
             $homepageEvents->the_post(); ?>
